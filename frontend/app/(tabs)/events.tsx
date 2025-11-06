7bc568575250e80a93e738d486477f19b323c220
milasera <serafm2@mcmaster.ca>
2025-11-05 23:34:06 -0500

    feat: filter-panel

--- frontend/app/(tabs)/events.tsx
+++ frontend/app/(tabs)/events.tsx
@@ -1,5 +1,8 @@
 import { View, Text, ScrollView } from 'react-native';
 import { FontAwesome } from "@expo/vector-icons";
+import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import { useMemo, useState } from "react";
+import { Slider } from '@miblanchard/react-native-slider';
 
 import { Link } from 'expo-router';
 
@@ -152,6 +155,10 @@ function InfoBox({show_name, artist, date, time, location, genre}: InfoBoxType) 
 
 
 export default function EventsScreen() {
+
+  const snapPoints = useMemo( () => ['10%', '50%'], []);
+  const [range, setRange] = useState<[number, number]>([20, 80]);  // set up state for ticket price slider bar
+
   return (
     <View className="flex-1 flex-col justify-start items-center bg-[#FFF0E2]">
       <ScrollView contentContainerStyle={{paddingTop: 120, paddingBottom: 150, alignItems: 'center'}}>
@@ -163,8 +170,76 @@ export default function EventsScreen() {
         <InfoBox show_name="Show Name" artist="Artist" date="Dec 24" time="8:00pm" location="Location" genre="genre"/>
       </ScrollView>
       {/* Rectangle 1 */}
-      
+    
+     {/* Bottom panel */}
+      <BottomSheet snapPoints={snapPoints} 
+        backgroundStyle={{ backgroundColor: '#F6D0AE' }}
+        handleIndicatorStyle={{ backgroundColor: '#FFF0E2' }} >
+        <BottomSheetView style={{ padding: 16 }} >
 
+          <Text style={{ marginBottom: 10 }}>
+            Ticket price:
+          </Text>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
+            {/* Min ticket price */}
+            <Text>
+              ${range[0].toFixed(0)}
+            </Text>
+
+            {/* Max ticket price */}
+            <Text>
+              ${range[1].toFixed(0)}
+            </Text>
+          </View>
+
+
+          {/* Slider bar for ticket price range */}
+          <Slider
+            value={range} 
+            onValueChange={(value: number | number[]) => {
+              // Type assertion for the range (always a 2-tuple of numbers even if the numbers are equal)
+              const rangeValue: [number, number] = Array.isArray(value)
+                ? [value[0], value[1]] 
+                : [value, value]; 
+              setRange(rangeValue);
+            }}
+            minimumValue={0}
+            maximumValue={100}
+            step={1}
+            minimumTrackTintColor="#FFF0E2"
+            maximumTrackTintColor="#FFF0E2"
+            thumbTintColor="#E8894A"
+          />
+
+        {/* Calendar buttons */}
+        <View
+          style={{
+            flexDirection: 'row',       
+            justifyContent: 'space-between', 
+            alignItems: 'center',       
+          }}
+        >
+          <View
+            style={{
+              width: 174,
+              height: 45,
+              backgroundColor: '#FFF0E2',
+              borderRadius: 8,
+            }}
+          />
+          <View
+            style={{
+              width: 174,
+              height: 45,
+              backgroundColor: '#FFF0E2',
+              borderRadius: 8,
+            }}
+          />
+        </View>
+
+      </BottomSheetView>
+      </BottomSheet>
     </View>
 
   );
