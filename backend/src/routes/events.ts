bd03e2c5e2dc8a659b31e408704172c8e90e0039
Austin Bray <93416177+AustinBray77@users.noreply.github.com>
2025-11-10 19:45:13 -0500

    Added custom parsing to deal with javascript wackyness

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -1,38 +1,42 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { eventService } from "../services/eventService.js";
+import { parseDateOr, parseFloatOr, parseIntOr } from "../utils/parser.js";
 
 const router = express.Router();
 
 // GET /api/events - Get all events matching given parameters
 router.get("/", async (req: Request, res: Response) => {
-    const limit = req.query.limit ? parseInt(req.query.limit as string) : 10;
+    const limit = parseIntOr(req.query.limit as string | undefined, 10);
 
-    const min_start_time = req.query.min_start_time
-        ? new Date(req.query.min_start_time as string)
-        : new Date("1970-1-1");
+    const min_start_time = parseDateOr(
+        req.query.min_start_time as string | undefined,
+        new Date("1970-1-1")
+    );
 
-    const max_start_time = req.query.max_start_time
-        ? new Date(req.query.max_start_time as string)
-        : new Date("2999-1-1");
+    const max_start_time = parseDateOr(
+        req.query.max_start_time as string | undefined,
+        new Date("2999-1-1")
+    );
 
-    const min_cost = req.query.min_cost
-        ? parseFloat(req.query.min_cost as string)
-        : 0;
+    const min_cost = parseFloatOr(req.query.min_cost as string | undefined, 0);
 
-    const max_cost = req.query.max_cost
-        ? parseFloat(req.query.max_cost as string)
-        : Number.MAX_VALUE;
+    const max_cost = parseFloatOr(
+        req.query.max_cost as string | undefined,
+        Number.MAX_VALUE
+    );
 
-    const latitude = req.query.latitude
-        ? parseFloat(req.query.latitude as string)
-        : 43.2636408; // Hamilton, ON
+    const latitude = parseFloatOr(
+        req.query.latitude as string | undefined,
+        43.2636408 // Hamilton, ON
+    );
 
-    const longitude = req.query.longitude
-        ? parseFloat(req.query.longitude as string)
-        : -79.9089415; // Hamilton, ON
+    const longitude = parseFloatOr(
+        req.query.longitude as string | undefined,
+        -79.9089415 // Hamilton, ON
+    );
 
-    const radius = req.query.radius ? parseInt(req.query.radius as string) : 10;
+    const radius = parseIntOr(req.query.radius as string | undefined, 10);
 
     eventService
         .getAllEvents(
